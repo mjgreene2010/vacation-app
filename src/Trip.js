432073c9ea139213a3c87dc0a893662d1ff5bde3
github username <github email address>
2019-07-23 23:53:42 -0500

    last commit of the night

--- src/Trip.js
+++ src/Trip.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 const trip = props => {
 
-    console.log(props.tripName)
+    console.log(props.addName)
 
     return <div>
         Name: <input onChange={props.addName}></input>{' '}
-        <button> Enter </button>
+        <button onClick={props.addName} > Enter </button>
+
+        {props.tripName.map(name => {
+            return <div>
+                <ol>
+                    {name}
+                </ol>
+            </div>
+        })}
+
     </div>
 }
 
